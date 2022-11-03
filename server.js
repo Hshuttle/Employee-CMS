225a206fbfd3b9743396d13d0fb3fc7bcef37b0d
Hshuttle <huntershutle@gmail.com>
2022-11-03 14:56:36 -0400

    wrote questions using inquirer and wrote inquirer function

--- server.js
+++ server.js
@@ -1,3 +1,27 @@
 const inquirer = require("inquirer");
 const mysql = require("mysql2");
 const table = require("console.table");
+
+const branchPrompt = [
+  {
+    type: "list",
+    message: "What would you like to do?",
+    choices: [
+      "View All Employees",
+      "Add Employee",
+      "Update Employee Role",
+      "View All Roles",
+      "Add Role",
+      "View All Departments",
+      "Add Department",
+      "Quit-Nothing Else To Do",
+    ],
+    name: "branchprompt",
+  },
+];
+
+function prompt() {
+  inquirer.prompt(branchPrompt).then((answers) => console.log(answers));
+}
+
+prompt();
