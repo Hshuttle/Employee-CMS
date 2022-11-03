fa075813442127c1ecbd4f3fd7cf6c08ec194086
Hshuttle <huntershutle@gmail.com>
2022-11-03 15:19:35 -0400

    updated schema.sql and added foerigh and primary keys

--- db/schema.sql
+++ db/schema.sql
@@ -1,22 +1,32 @@
 CREATE DATABASE cms_db;
 USE cms_db;
 
+CREATE TABLE department (
+   id: INT PRIMARY KEY,
+   department_name: VARCHAR(30)
+   ON DELETE SET NULL
+)
+
+CREATE TABLE roles (
+  id: INT PRIMARY KEY,
+  title: VARCHAR(30),
+  salary: DECIMAL,
+  department_name: INT
+  FOREIGN KEY (department_name)
+  REFERENCES department(department_name)
+  ON DELETE SET NULL
+)
+
 CREATE TABLE employee (
   id: INT PRIMARY KEY,
   first_name: VARCHAR(30),
   last_name: VARCHAR(30),
   role_id: INT,
   manager_id: INT
+  FOREIGN KEY (role_id)
+  REFERENCES roles(id)
+  ON DELETE SET NULL
 );
 
-CREATE TABLE roles (
-  id: INT PRIMARY KEY,
-  title: VARCHAR(30),
-  salary: DECIMAL,
-  department_id: INT
-)
 
-CREATE TABLE department (
-   id: INT PRIMARY KEY,
-   name: VARCHAR(30)
-)
+
