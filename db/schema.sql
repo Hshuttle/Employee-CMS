7e64b22da81fc530b5881e696c11ec0c108310b0
Hshuttle <huntershutle@gmail.com>
2022-11-05 11:58:10 -0400

    switched out console.table for console table printer because it looks nicer, added my view table functions with if statements depending on answers

--- db/schema.sql
+++ db/schema.sql
@@ -5,25 +5,27 @@ USE cms_db;
 CREATE TABLE department (
    id INT AUTO_INCREMENT PRIMARY KEY,
    department_name VARCHAR(30)
-)
+);
 
 CREATE TABLE roles (
   id INT AUTO_INCREMENT PRIMARY KEY,
   title VARCHAR(30),
   salary DECIMAL,
-  department_name INT
-  FOREIGN KEY (department_name)
-  REFERENCES department(department_name)
-)
+  department_id INT,
+  FOREIGN KEY (department_id)
+  REFERENCES department(id)
+);
 
 CREATE TABLE employee (
   id INT AUTO_INCREMENT PRIMARY KEY,
   first_name VARCHAR(30),
   last_name VARCHAR(30),
   role_id INT,
-  manager_id INT
+  manager_id INT,
   FOREIGN KEY (role_id)
-  REFERENCES roles(id)
+  REFERENCES roles(id),
+  FOREIGN KEY (manager_id)
+  REFERENCES employee(id)
 );
 
 
--- server.js
+++ server.js
@@ -1,6 +1,16 @@
 const inquirer = require("inquirer");
 const mysql = require("mysql2");
-const table = require("console.table");
+const { printTable } = require('console-table-printer');
+
+const db = mysql.createConnection(
+  {
+    host: "localhost",
+    user: "root",
+    password: "Hunt2342!",
+    database: "cms_db",
+  },
+  console.log(`Connected to the classlist_db database.`)
+);
 
 const branchPrompt = [
   {
@@ -16,12 +26,63 @@ const branchPrompt = [
       "Add Department",
       "Quit-Nothing Else To Do",
     ],
-    name: "branchprompt",
+    name: "branchPrompt",
   },
 ];
 
 function prompt() {
-  inquirer.prompt(branchPrompt).then((answers) => console.log(answers));
+  inquirer.prompt(branchPrompt).then((answers) => {
+    if (answers.branchPrompt === "View All Employees") {
+      viewEmployees();
+    } else if (answers.branchPrompt === "View All Roles") {
+        viewRoles();
+    } else if (answers.branchPrompt === "View All Departments") {
+        viewDepartment();
+    } else{addDepartment()}
+  });
 }
+
+function viewEmployees() {
+  db.promise()
+    .query("SELECT CONCAT(employee.first_name,' ',employee.last_name) AS Name, roles.title AS Title, roles.salary AS Salary, department.department_name AS Department, CONCAT(manager.first_name,' ',manager.last_name) AS Manager FROM employee LEFT JOIN roles ON employee.role_id = roles.id LEFT JOIN department ON roles.department_id = department.id LEFT JOIN employee manager ON manager.id = employee.manager_id")
+    .then(([response]) => {
+        printTable(response)
+        prompt()
+    }
+    );
+}
+function viewRoles() {
+    db.promise()
+      .query("SELECT roles.title AS Title, roles.salary AS Salary, department.department_name AS Department FROM roles LEFT JOIN department ON roles.department_id = department.id")
+      .then(([response]) => {
+          printTable(response)
+          prompt()
+      }
+      );
+  }
+  function viewDepartment() {
+    db.promise()
+      .query("SELECT * FROM department")
+      .then(([response]) => {
+          printTable(response)
+          prompt()
+      }
+      );
+  }
+
+  function addDepartment(){
+    inquirer.prompt({type: "input", name: "name", message: "What is the name of the new department"}).then(answers => {
+        db.promise().query("INSERT INTO department SET ?", {department_name: answers.name}).then(([response]) => {
+            if(response.affectedRows > 0){
+                viewDepartment()
+            } else {
+                console.log("failed to create department");
+                prompt()
+            }
+            
+        }
+        );
+    })
+  }
 
 prompt();
