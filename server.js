6d91dd7a5eaf321fe225570ffd300c1da24a03f2
Hshuttle <huntershutle@gmail.com>
2022-11-05 20:45:59 -0400

    added new role and new employee functions and modified if statement to include these

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const inquirer = require("inquirer");
 const mysql = require("mysql2");
-const { printTable } = require('console-table-printer');
+const table = require("console.table");
 
 const db = mysql.createConnection(
   {
@@ -35,54 +35,133 @@ function prompt() {
     if (answers.branchPrompt === "View All Employees") {
       viewEmployees();
     } else if (answers.branchPrompt === "View All Roles") {
-        viewRoles();
+      viewRoles();
     } else if (answers.branchPrompt === "View All Departments") {
-        viewDepartment();
-    } else{addDepartment()}
+      viewDepartment();
+    } else if (answers.branchPrompt === "Add Department") {
+      addDepartment();
+    } else if (answers.branchPrompt === "Add Employee") {
+      addEmployee();
+    } else if (answers.branchPrompt === "Add Role") {
+      addRole();
+    } else {
+      return console.log("END");
+    }
   });
 }
 
 function viewEmployees() {
   db.promise()
-    .query("SELECT CONCAT(employee.first_name,' ',employee.last_name) AS Name, roles.title AS Title, roles.salary AS Salary, department.department_name AS Department, CONCAT(manager.first_name,' ',manager.last_name) AS Manager FROM employee LEFT JOIN roles ON employee.role_id = roles.id LEFT JOIN department ON roles.department_id = department.id LEFT JOIN employee manager ON manager.id = employee.manager_id")
+    .query(
+      "SELECT CONCAT(employee.first_name,' ',employee.last_name) AS Name, roles.title AS Title, roles.salary AS Salary, department.department_name AS Department, CONCAT(manager.first_name,' ',manager.last_name) AS Manager FROM employee LEFT JOIN roles ON employee.role_id = roles.id LEFT JOIN department ON roles.department_id = department.id LEFT JOIN employee manager ON manager.id = employee.manager_id"
+    )
     .then(([response]) => {
-        printTable(response)
-        prompt()
-    }
-    );
+      console.table(response);
+      prompt();
+    });
 }
 function viewRoles() {
-    db.promise()
-      .query("SELECT roles.title AS Title, roles.salary AS Salary, department.department_name AS Department FROM roles LEFT JOIN department ON roles.department_id = department.id")
-      .then(([response]) => {
-          printTable(response)
-          prompt()
-      }
-      );
-  }
-  function viewDepartment() {
-    db.promise()
-      .query("SELECT * FROM department")
-      .then(([response]) => {
-          printTable(response)
-          prompt()
-      }
-      );
-  }
+  db.promise()
+    .query(
+      "SELECT roles.title AS Title, roles.salary AS Salary, department.department_name AS Department FROM roles LEFT JOIN department ON roles.department_id = department.id"
+    )
+    .then(([response]) => {
+      console.table(response);
+      prompt();
+    });
+}
+function viewDepartment() {
+  db.promise()
+    .query("SELECT * FROM department")
+    .then(([response]) => {
+      console.table(response);
+      prompt();
+    });
+}
 
-  function addDepartment(){
-    inquirer.prompt({type: "input", name: "name", message: "What is the name of the new department"}).then(answers => {
-        db.promise().query("INSERT INTO department SET ?", {department_name: answers.name}).then(([response]) => {
-            if(response.affectedRows > 0){
-                viewDepartment()
-            } else {
-                console.log("failed to create department");
-                prompt()
-            }
-            
-        }
-        );
+function addDepartment() {
+  inquirer
+    .prompt({
+      type: "input",
+      name: "name",
+      message: "What is the name of the new department",
     })
-  }
+    .then((answers) => {
+      db.promise()
+        .query("INSERT INTO department SET ?", {
+          department_name: answers.name,
+        })
+        .then(([response]) => {
+          if (response.affectedRows > 0) {
+            viewDepartment();
+          } else {
+            console.log("failed to create department");
+            prompt();
+          }
+        });
+    });
+}
+
+function addRole() {
+  inquirer
+    .prompt(
+      {
+        type: "input",
+        name: "title",
+        message: "What is the new roles title?",
+      },
+      {
+        type: "input",
+        name: "salary",
+        message: "What is the new roles salary?",
+      }
+    )
+    .then((answers) => {
+      db.promise()
+        .query("INSERT INTO roles SET ?", {
+          title: answers.title,
+          salary: answers.salary,
+        })
+        .then(([response]) => {
+          if (response.affectedRows > 0) {
+            viewRoles();
+          } else {
+            console.log("failed to create new role");
+            prompt();
+          }
+        });
+    });
+}
+
+function addEmployee() {
+  inquirer
+    .prompt(
+      {
+        type: "input",
+        name: "first",
+        message: "What is the new employees first name?",
+      },
+      {
+        type: "input",
+        name: "last",
+        message: "What is the new employees last name?",
+      }
+    )
+    .then((answers) => {
+      db.promise()
+        .query("INSERT INTO roles SET ?", {
+          first_name: answers.first,
+          last_name: answers.last,
+        })
+        .then(([response]) => {
+          if (response.affectedRows > 0) {
+            viewEmployees();
+          } else {
+            console.log("failed to create new employee");
+            prompt();
+          }
+        });
+    });
+}
 
 prompt();
